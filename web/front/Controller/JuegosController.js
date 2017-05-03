39b8bbcf41875dce55a40b87624875fd78b5be8a
berto <albertomartinnot@gmail.com>
2017-05-03 16:55:17 +0200

    Delete Alert message

--- web/front/Controller/JuegosController.js
+++ web/front/Controller/JuegosController.js
@@ -24,28 +24,23 @@ juegos.config(function($routeProvider){
 juegos.controller('JuegosController', function($http,$scope){
 	$scope.juego={};
 	$scope.info=false;
-
+	$scope.alerta=false;
 	$scope.mostrar=function(){
+		$scope.alerta=false;
 		$http.get('http://localhost:8000/list').then(function(response){
-
 			$scope.juego=response.data;
-
-
 		});
 
 		
 	}
 
 	$scope.borrar=function($id){
-		console.log($id);
+		$scope.alerta=true;
 		$http.delete('http://localhost:8000/delete/'+$id).then(location.reload());
-
-
 
 	}
 
 	$scope.add=function($juego){
-		
 		$game=$juego;
 		$http.post('http://localhost:8000/add',$game).then(location.replace('http://localhost:8000/front/index.html#!/lista'));
 
@@ -53,7 +48,6 @@ juegos.controller('JuegosController', function($http,$scope){
 	}	
 
 	$scope.edit=function($juego,$id){
-		
 		$game=$juego;
 		$num=$id;
 		$http.put('http://localhost:8000/put/'+$num,$game).then(location.replace('http://localhost:8000/front/index.html#!/lista'));
